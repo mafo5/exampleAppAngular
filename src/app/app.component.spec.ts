687bf4493cbb4fd27f58829968ce57992b2cf648
Martin Fochler <mafo.work@googlemail.com>
2020-03-31 13:24:27 +0200

    refactor tests to given when then pattern and hamjest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import * as __ from 'hamjest';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let compiled: HTMLElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -12,24 +18,26 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
+
+    fixture.detectChanges();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+
+    __.assertThat(component, __.is(__.truthy()));
   });
 
   it(`should have as title 'exampleApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('exampleApp');
+
+    __.assertThat(component, __.hasProperty('title', __.is('exampleApp')));
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('exampleApp app is running!');
+
+    __.assertThat(compiled.querySelector('.content span'), __.hasProperty('textContent', __.containsString('exampleApp app is running!')));
   });
 });
