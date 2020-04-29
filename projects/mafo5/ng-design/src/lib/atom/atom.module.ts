4b67e6e7789591a5e4e0490b10502f0ef881478c
Martin Fochler <mafo.work@googlemail.com>
2020-04-29 10:25:00 +0200

    fixing reference issues with libraries

--- projects/mafo5/ng-design/src/lib/atom/atom.module.ts
+++ projects/mafo5/ng-design/src/lib/atom/atom.module.ts
@@ -3,18 +3,32 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IconComponent } from './icon/icon.component';
+import { ButtonComponent } from './button/button.component';
+import { HeadlineComponent } from './headline/headline.component';
+import { InputComponent } from './input/input.component';
+import { LabelComponent } from './label/label.component';
+import { ListComponent } from './list/list.component';
+import { TextareaComponent } from './textarea/textarea.component';
+
+const componentList = [
+  ButtonComponent,
+  HeadlineComponent,
+  IconComponent,
+  InputComponent,
+  LabelComponent,
+  ListComponent,
+  TextareaComponent,
+];
 
 @NgModule({
-  declarations: [
-    IconComponent,
-  ],
+  declarations: componentList,
   imports: [
     CommonModule,
     FormsModule,
   ],
   exports: [
-    IconComponent,
     FormsModule,
+    ...componentList,
   ]
 })
 export class AtomModule { }
--- projects/mafo5/ng-design/src/lib/atom/index.ts
+++ projects/mafo5/ng-design/src/lib/atom/index.ts
@@ -1,3 +1,9 @@
+export * from './button/button.component';
+export * from './headline/headline.component';
 export * from './icon/icon.component';
+export * from './input/input.component';
+export * from './label/label.component';
+export * from './list/list.component';
+export * from './textarea/textarea.component';
 
 export * from './atom.module';
