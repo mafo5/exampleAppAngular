d2ac16a0ecc3f825de21f8bd35212a86d992e501
Martin Fochler <mafo.work@googlemail.com>
2020-04-29 09:15:39 +0200

    remove all old references of design module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DesignModule } from '@example-app-angular/design';
+import { DesignModule } from '@mafo5/ng-design';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
